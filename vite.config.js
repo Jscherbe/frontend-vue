df8c648ca16522940ce7d735bde05d0d9e3aa034
Joseph Scherben <jscherbe@gmail.com>
2024-01-15 14:27:57 -0500

    Remove histoire for testing stroybook

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-/// <reference types="histoire" />
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDocgenPlugin from 'vite-plugin-vue-docgen';
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueDocgenPlugin()
   ]
 });
