7c0f32b62268baebba70d9a98e6782994c429d83
Joe Scherben <joe.scherben@informatics-studio.com>
2026-03-25 12:58:37 -0400

    Minor cleanup

--- lib/components/collapsible/UluAccordionGroup.vue
+++ lib/components/collapsible/UluAccordionGroup.vue
@@ -31,53 +31,53 @@
 </template>
 
 <script setup>
-import { ref, provide } from 'vue';
-import UluAccordion from './UluAccordion.vue';
+  import { ref, provide } from 'vue';
+  import UluAccordion from './UluAccordion.vue';
 
-const props = defineProps({
-  /**
-   * Array of items to render as accordions.
-   * Each item can have: title, content, isOpen, classes
-   */
-  items: {
-    type: Array,
-    default: () => []
-  },
-  /**
-   * If using summary text sets the inner element the text is wrapped in, usually a headline or strong
-   */
-  triggerTextElement: {
-    type: String,
-    default: "strong"
-  },
-  /**
-   * Class modifiers (ie. 'transparent', 'secondary', etc)
-   */
-  modifiers: [String, Array],
-  /**
-   * Enable or configure animations.
-   * - `false` (default) to disable all animations.
-   * - `true` to enable animations with default settings.
-   * - An object to provide custom options to auto-animate (e.g., { duration: 100, easing: 'linear' }).
-   */
-  animate: {
-    type: [Boolean, Object],
-    default: true
-  },
-});
+  const props = defineProps({
+    /**
+     * Array of items to render as accordions.
+     * Each item can have: title, content, isOpen, classes
+     */
+    items: {
+      type: Array,
+      default: () => []
+    },
+    /**
+     * If using summary text sets the inner element the text is wrapped in, usually a headline or strong
+     */
+    triggerTextElement: {
+      type: String,
+      default: "strong"
+    },
+    /**
+     * Class modifiers (ie. 'transparent', 'secondary', etc)
+     */
+    modifiers: [String, Array],
+    /**
+     * Enable or configure animations.
+     * - `false` (default) to disable all animations.
+     * - `true` to enable animations with default settings.
+     * - An object to provide custom options to auto-animate (e.g., { duration: 100, easing: 'linear' }).
+     */
+    animate: {
+      type: [Boolean, Object],
+      default: true
+    },
+  });
 
-const activeAccordionId = ref(null);
+  const activeAccordionId = ref(null);
 
-function toggle(id, isOpen) {
-  if (isOpen) {
-    activeAccordionId.value = id;
-  } else if (activeAccordionId.value === id) {
-    activeAccordionId.value = null;
+  function toggle(id, isOpen) {
+    if (isOpen) {
+      activeAccordionId.value = id;
+    } else if (activeAccordionId.value === id) {
+      activeAccordionId.value = null;
+    }
   }
-}
 
-provide('uluAccordionGroup', {
-  activeAccordionId,
-  toggle
-});
+  provide('uluAccordionGroup', {
+    activeAccordionId,
+    toggle
+  });
 </script>
